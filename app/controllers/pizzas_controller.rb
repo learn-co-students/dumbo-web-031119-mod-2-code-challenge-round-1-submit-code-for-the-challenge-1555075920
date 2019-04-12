75bbb3b24cab246b612d5fc496fc9bfea4e4e3f0
Danielle Alba <danib.sgh@gmail.com>
2019-04-12 11:14:33 -0400

    couldnt figure out the search bar

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -1,5 +1,38 @@
 class PizzasController < ApplicationController
+# access and call my find_pizza method with before_action
+before_action :find_pizza, only: [:show, :edit]
+
   def index
     @pizzas = Pizza.all
   end
+
+  def show
+  end
+
+  def new
+    @pizza = Pizza.new
+  end
+
+  def create
+    @pizza = Pizza.create(pizza_params)
+    if @pizza.valid?
+      redirect_to pizza_path(@pizza)
+    else
+      render :new
+    end
+  end
+
+
+
+  private
+  # secure params
+  def pizza_params
+    params.require(:pizza).permit(:name, :ingredients, :restaurant_id)
+  end
+
+  def find_pizza
+    @pizza = Pizza.find(params[:id])
+  end
+
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,12 @@
 class Restaurant < ApplicationRecord
-  
+  has_many :pizzas
+
+# trying to figure out seach bar, google brought this up
+  def self.search(search)
+  if search
+    find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
+  else
+    find(:all)
+  end
+end
 end
