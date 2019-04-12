31c16237a4e544da20f9eb7f01c1cb5c31f07ce5
Jeff Yu <jwy5140@gmail.com>
2019-04-12 11:11:40 -0400

    Unfinished Search Bar (Reason for Error)

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,2 +1,12 @@
 class Pizza < ApplicationRecord
+    belongs_to :restaurant, dependent: :destroy
+
+    validates :name, uniqueness: true
+    validates :name, presence: true
+    validates :ingredients, presence: true
+    validates :restaurant_id, presence: true
+
+    def ingredients_arr
+        self.ingredients.split("\n")
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get '/restaurants', to: 'restaurants#index'
+  get '/restaurants/search/:name', to: 'restaurants#show'
+  resources :restaurants, only: [:show]
   get '/pizzas', to: 'pizzas#index'
+  resources :pizzas, only: [:show, :new, :create]
 end
