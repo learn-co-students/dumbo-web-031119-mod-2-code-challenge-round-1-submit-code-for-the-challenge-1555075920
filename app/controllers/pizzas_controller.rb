effe8e0dfe8d5ebe4dd3d63f8fb2b4ee072edbae
beschwa <naraiswar@gmail.com>
2019-04-12 11:07:02 -0400

    Submission

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -1,5 +1,34 @@
 class PizzasController < ApplicationController
-  def index
-    @pizzas = Pizza.all
-  end
+
+	before_action :get_pizza, only: [:show, :edit, :update]
+
+	def index
+		@pizzas = Pizza.all
+	end
+
+	def show
+	end
+
+	def new
+		@pizza = Pizza.new
+		@restaurants = Restaurant.all
+	end
+
+	def create
+		@pizza = Pizza.new(strong_params(:name, :ingredients, :restaurant_id))
+		@pizza.save ? (redirect_to pizza_path(@pizza)) : (render :new)
+	end
+
+
+
+
+	private
+
+	def get_pizza
+		@pizza = Pizza.find(params[:id])
+	end
+
+	def strong_params (*args)
+		params.require(:pizza).permit(*args)
+	end
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,26 @@
 class RestaurantsController < ApplicationController
-  def index
-    @restaurants = Restaurant.all
-  end
+
+	before_action :get_restaurant, only: [:show]
+
+
+	def index
+		params[:search].nil? ? @restaurants = (Restaurant.all) : (@restaurants = Restaurant.search(strong_search))
+	end
+
+	def show
+		@pizzas = @restaurant.pizzas
+	end
+
+
+
+
+	private
+
+	def get_restaurant
+		@restaurant = Restaurant.find(params[:id])
+	end
+
+	def strong_search
+		params.permit(:search)
+	end
 end
