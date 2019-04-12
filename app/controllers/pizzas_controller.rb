d28df41c4c0fbecffac703f0a42f512d04e1de69
John <jpena91@gmail.com>
2019-04-12 11:11:51 -0400

    done

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -2,4 +2,40 @@ class PizzasController < ApplicationController
   def index
     @pizzas = Pizza.all
   end
+
+  def show
+    @pizza = Pizza.find(params[:id])
+  end
+
+  def new
+    @pizza = Pizza.new
+    @restaurants = Restaurant.all
+  end
+
+  def create
+    @pizza = Pizza.create(strong_params)
+    if @pizza.valid?
+      @pizza.save
+      redirect_to restaurants_path
+    else
+      render :new
+    end
+  end
+
+
+  def show
+    @pizza = Pizza.find(params[:id])
+    @restaurant = @pizza.restaurant
+  end
+
+
+
+  private
+
+
+  def strong_params
+    params.require(:pizza).permit(:name, :ingredients, :restaurant_id)
+  end
+
+
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,20 @@
 class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
+    # @restaurant = Project.search(params[:search])
   end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    @pizza = @restaurant.pizzas
+  end
+
+  def new
+    Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.create(rest_params)
+  end
+
 end
