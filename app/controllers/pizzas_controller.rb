2571f071792cb619bbd640897cc6d16e3e04140f
Jordan Rivera <jordan.rivera34@gmail.com>
2019-04-12 11:17:08 -0400

    DONE

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -1,5 +1,27 @@
 class PizzasController < ApplicationController
+
   def index
     @pizzas = Pizza.all
+  end
+
+  def show
+    @pizza = Pizza.find(params[:id])
+  end
+
+  def new
+    @pizza = Pizza.new
+    @restaurants = Restaurant.all
+  end
+
+  def create
+    @pizza = Pizza.create(pizza_params)
+    redirect_to pizza_path(@pizza)
+  end
+
+
+  private
+
+  def pizza_params
+    params.require(:pizza).permit(:name, :ingredients)
   end
 end
