6a564e8775bda2fba86609246cb6cb326fb605c2
github username <github email address>
2019-04-12 11:11:27 -0400

    done

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,2 +1,14 @@
 class Pizza < ApplicationRecord
+  belongs_to :restaurant
+  validates :name,uniqueness:true,presence:true
+  validates :ingredients,presence:true
+
+  def pizza_name
+  category.try(:name)
+  end
+
+  def pizza_name=(name)
+  self.category = Category.find_by(name: name) if name.present?
+  end
+
 end
