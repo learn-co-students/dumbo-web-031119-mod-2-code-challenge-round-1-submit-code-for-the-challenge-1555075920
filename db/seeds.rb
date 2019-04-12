d85c4fb992dc6d2b478c05ef3c92ca41d20c7379
ashhcashh1120 <aarchibald20@gmail.com>
2019-04-12 11:16:03 -0400

    Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,16 +8,18 @@
 #
 #   1
 Restaurant.destroy_all
+Pizza.destroy_all
  
-Restaurant.create!([{
-  name: "Sottocasa NYC",
-  address: "298 Atlantic Ave, Brooklyn, NY 11201",
-},
-{
-  name: "PizzArte",
-  address: "69 W 55th St, New York, NY 10019",
-},
-{
-  name: "San Matteo NYC",
-  address: "1559 2nd Ave, New York, NY 10028"
-}])
+Restaurant.create(name: "Sottocasa NYC",address: "298 Atlantic Ave, Brooklyn, NY 11201")
+
+Restaurant.create(name: "PizzArte",address: "69 W 55th St, New York, NY 10019")
+
+Restaurant.create(name: "San Matteo NYC",address: "1559 2nd Ave, New York, NY 10028")
+
+
+Pizza.create(name:"Cheese", ingredients:"cheese", restaurant_id:1)
+Pizza.create(name:"test5", ingredients:"c", restaurant_id: 2)
+Pizza.create(name:"test4", ingredients:"ch", restaurant_id: 3)
+Pizza.create(name:"test3", ingredients:"bw", restaurant_id: 1)
+Pizza.create(name:"test2", ingredients:"cd", restaurant_id: 2)
+Pizza.create(name:"test1", ingredients:"che", restaurant_id: 3)
