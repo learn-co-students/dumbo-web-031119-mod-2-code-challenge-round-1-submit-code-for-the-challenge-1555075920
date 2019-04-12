e43bec8951339a7ca7ffc477482b23058e195b96
erodxd <emrod718@gmail.com>
2019-04-12 11:11:42 -0400

    Done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,16 @@
 class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
+    
+    if params[:restaurant]
+      @restaurant = Roupon.where('restaurant LIKE ?', "%#{params[:restaurant]}%")
+    else
+      @restaurant = Restaurant.all
+    end
   end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
 end
